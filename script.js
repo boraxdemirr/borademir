d9a794500b868d1ee371838dbe185c37320ff8b0
Boraxdemirr <borad518@gmail.com>
2025-05-11 01:59:53 +0300

    Dosyalar Eklendi

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded",function () {
-    const btn = document.querySelector(".btn");
-    const singer = document.querySelector(".sng");
-   
-    btn.addEventListener("click", () => {
-        const sngvalue = singer.value;
-        alert(sngvalue)
-    })
-})
